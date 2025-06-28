bc091651ec560ccd20cb54d4a03808888c50a840
000Gaurav-Chauhan000 <shubhamchauhan35725@gmail.com>
2025-06-28 09:39:21 +0530

    Update Person.java

--- Constructor/Level1/Person.java
+++ Constructor/Level1/Person.java
@@ -2,13 +2,11 @@ public class Person {
     private String name;
     private int age;
 
-    // Parameterized constructor
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    // Copy constructor
     public Person(Person other) {
         this.name = other.name;
         this.age = other.age;
